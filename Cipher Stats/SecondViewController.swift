2bb4f477ab7c66185419bd0f7358efd020e7f7f0
Dylan Rothfeld <drothfel@gmail.com>
2019-07-07 16:23:40 -0700

    initial commit

---
+++ Cipher Stats/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Cipher Stats
+//
+//  Created by Dylan Rothfeld on 7/7/19.
+//  Copyright © 2019 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
