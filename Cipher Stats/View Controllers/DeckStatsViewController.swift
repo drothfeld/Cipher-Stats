6f321db31ae1c65c2cddad474fc985158c96040e
Dylan Rothfeld <drothfel@gmail.com>
2019-08-14 16:22:03 -0700

    textfield ui elements on DeckStatsViewController to make FIrebase API call

--- Cipher Stats/View Controllers/DeckStatsViewController.swift
+++ Cipher Stats/View Controllers/DeckStatsViewController.swift
@@ -13,9 +13,26 @@ import UIKit
 // The view controller for selecting and viewing
 // cipher MC deck statistics.
 // ==================================================
-class DeckStatsViewController: UIViewController {
+class DeckStatsViewController: UIViewController, UITextFieldDelegate {
+    // Storyboard Outlets
+    @IBOutlet weak var DeckTextfield: UITextField!
+    @IBOutlet weak var OpponentDeckTextfield: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        DeckTextfield.attributedPlaceholder = NSAttributedString(string: "Enter Deck", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        OpponentDeckTextfield.attributedPlaceholder = NSAttributedString(string: "Enter Opponent Deck", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+    }
+    
+    // Hide keyboard when done key is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    @IBAction func deckNameChanged(sender: UITextField) {
+        // TODO: Make Firebase API call to get
+        //       entered deck and opponent statistics
     }
 }
